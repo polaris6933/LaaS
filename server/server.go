b32e190b49b5d1811bcd42702edd1a53fad4945e
polaris6933 <zdravko@witlogic.com>
2019-01-23 22:48:49 +0200

    server: implement adding and removing sessions

--- server/server.go
+++ server/server.go
@@ -3,18 +3,65 @@ package main
 import (
 	"LaaS/executor"
 	"bufio"
+	"crypto/sha256"
 	"fmt"
 	"net"
 	"os"
 	"strings"
+	"time"
 )
 
-func add(args []string, conection *net.Conn) {
-	fmt.Println("add()", args)
+var sessions []*session
+
+const timeFormat string = "Mon Jan 2 2006 15:04"
+
+type session struct {
+	name                  string
+	created               time.Time
+	passwordHash          [sha256.Size]byte
+}
+
+func NewSession(name string, password string) *session {
+	s := new(session)
+	s.name = name
+	s.created = time.Now()
+	s.passwordHash = sha256.Sum256([]byte(password))
+	return s
+}
+
+func (s *session) getStringRepresentation() string {
+	return "session " + s.name + ", created at " + s.created.Format(timeFormat)
+}
+
+func (s *session) String() string {
+	return fmt.Sprintf(s.getStringRepresentation())
+}
+
+func add(args []string, connection *net.Conn) {
+	name := args[0]
+	pass := args[1]
+	for _, session := range sessions {
+		if session.name == name {
+			(*connection).Write(
+				[]byte("session with the name " + name + "already exists"))
+			return
+		}
+	}
+	sessions = append(sessions, NewSession(name, pass))
+	fmt.Println((*connection).RemoteAddr().String(), "created session", name)
 }
 
 func remove(args []string, conection *net.Conn) {
-	fmt.Println("remove()", args)
+	name := args[0]
+	for idx, session := range sessions {
+		if session.name == name {
+			last := len(sessions) - 1
+			sessions[idx] = sessions[last]
+			sessions = sessions[:last]
+			return
+		}
+	}
+	fmt.Println("no session with the name", name, "found")
 }
 
 func pause(args []string, conection *net.Conn) {
@@ -29,8 +76,11 @@ func export(args []string, conection *net.Conn) {
 	fmt.Println("export()", args)
 }
 
-func list(args []string, conection *net.Conn) {
-	fmt.Println("list()", args)
+func list(args []string, connection *net.Conn) {
+	for _, session := range sessions {
+		(*connection).Write([]byte(session.getStringRepresentation()))
+	}
+	fmt.Println(len(sessions), "total\n")
 }
 
 func handleRequest(e *executor.Executor) {
@@ -64,14 +114,16 @@ func main() {
 	defer l.Close()
 
 	var actions = map[string]executor.Action{
-		"add":    {add, 2},
-		"remove": {remove, 1},
-		"pause":  {pause, 1},
-		"resume": {resume, 1},
-		"export": {export, 2},
-		"list":   {list, 0},
+		"add":          {add, 2},
+		"remove":       {remove, 1},
+		"pause":        {pause, 1},
+		"resume":       {resume, 1},
+		"export":       {export, 2},
+		"list":         {list, 0},
+		"authenticate": {authenticate, 1},
 	}
 
+	sessions = []*session{}
 	executor := executor.NewExecutor(actions)
 
 	for {
