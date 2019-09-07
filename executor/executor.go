9b56b6bae03addb44776076bbc215bc833d9596c
polaris6933 <zdravko.georgiev@protonmail.com>
2019-09-07 13:38:38 +0300

    executor: add documentation

--- executor/executor.go
+++ executor/executor.go
@@ -1,3 +1,5 @@
+// Package executor provides a mechanism for running methods of a given type
+// providing an instance of this type and the name of the method as a string.
 package executor
 
 import (
@@ -7,10 +9,19 @@ import (
 	"strings"
 )
 
+// A type must implement this interface in order for the executor to be able
+// to work with it. The method is a no-op, it's just needed to mark a type as
+// executable.
 type Executable interface {
 	AssertExecutable()
 }
 
+// Return a reflect object which, when executed, will run the method described
+// by `anyType` and `command` and nil. `command` should be a string containing
+// the method name and the arguments for that method.
+// An error is returned if:
+//   - `command` is not a method of the type of `anyType`
+//   - `command` does not contain enough arguments for the method it describes
 func Execute(anyType Executable, command string) ([]reflect.Value, error) {
 	commandSplit := strings.Split(command, " ")
 	commandName := commandSplit[0]
